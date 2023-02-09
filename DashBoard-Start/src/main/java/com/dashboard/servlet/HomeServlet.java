31368a36d3b3aa3667ae1724c4fb9d34171c3f58
Anthony Doban <doban911@aol.com>
2023-02-09 11:16:53 -0500

    Basic HTTPServlet response from do get method using paramater from reqest in url

---
+++ DashBoard-Start/src/main/java/com/dashboard/servlet/HomeServlet.java
@@ -0,0 +1,44 @@
+package com.dashboard.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HomeServlet
+ */
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// writer used to send content to browser via response body
+		// append (to the response body)
+		// get request for ip/home-> servlet that is mapped to /home (HomeServlet) -> doGet -> send response with name from request in body
+		response.getWriter().append(request.getParameter("name"));
+		//run on wildfly sever and navigate to http://localhost:8080/DashBoard-Start/home?name=ny
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
