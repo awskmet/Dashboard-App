462aa241e1bc18dac2d0fee5d24f4030094fcc06
Anthony Doban <doban911@aol.com>
2023-02-10 12:40:29 -0500

    Update to latest

--- DashBoard-Start/src/main/java/com/dashboard/servlet/toolservlets/AsyncServlet.java
+++ DashBoard-Start/src/main/java/com/dashboard/servlet/toolservlets/AsyncServlet.java
@@ -1,6 +1,8 @@
-package com.dashboard.servlet;
+package com.dashboard.servlet.toolservlets;
 
 import java.io.IOException;
+
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,17 +10,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class HomeServlet
+ * Servlet implementation class AsyncServlet
  */
 //for filter  would have to add asyncsuppoprted to same spot in filter class
-@WebServlet("/home")
-public class HomeServlet extends HttpServlet {
+@WebServlet(urlPatterns="/async", asyncSupported=true)
+public class AsyncServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public HomeServlet() {
+    public AsyncServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,10 +29,28 @@ public class HomeServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// writer used to send content to browser via response body
-		// redirect to home html
-		response.sendRedirect("home.html");
-		//run on wildfly sever and navigate to http://localhost:8080/DashBoard-Start/home?name=ny
+		AsyncContext asyncContext = request.startAsync();
+		response.getWriter().append("I'm thread one. Thread two is slow so he is separated\n");
+		//I happen in a separate thread
+		asyncContext.start(new Runnable() {
+			@Override
+			public void run() {
+				try {
+
+					response.getWriter().append("I'm thread two. I'm gonna take forever so I'm separated \n");
+					Thread.sleep(2000);
+					response.getWriter().append("I'm done (thread two)\n");
+					asyncContext.complete();
+				}catch(InterruptedException e) {
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+		});
+		response.getWriter().append("I'm much quicker and didn't want to wait for thread two to finish, so I'll go ahead and finish (thread one)\n");
 	}
 
 	/**
