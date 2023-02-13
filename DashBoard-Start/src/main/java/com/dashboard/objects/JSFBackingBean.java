15b51c4d626771a26d7e5c21e4cbf7f65c809868
Anthony Doban <doban911@aol.com>
2023-02-13 14:34:54 -0500

    Add fields, message, validation, formats

--- DashBoard-Start/src/main/java/com/dashboard/objects/JSFBackingBean.java
+++ DashBoard-Start/src/main/java/com/dashboard/objects/JSFBackingBean.java
@@ -1,18 +1,33 @@
 package com.dashboard.objects;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 import javax.annotation.ManagedBean;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
 
-@Named(value="test")
+@Named(value="jsfBean")
 @RequestScoped
 public class JSFBackingBean implements Serializable{
 private String beanusField;
+public String getValidatedField() {
+	return validatedField;
+}
+public void setValidatedField(String validatedField) {
+	this.validatedField = validatedField;
+}
+private String validatedField;
 private String deenusField;
+private LocalDate availDate;
 
+public LocalDate getAvailDate() {
+	return availDate;
+}
+public void setAvailDate(LocalDate availDate) {
+	this.availDate = availDate;
+}
 public String getBeanusField() {
 	return beanusField;
 }
